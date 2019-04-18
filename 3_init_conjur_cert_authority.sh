545c02cb953a287bb40b3e3ff118c4527a1e5912
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-04-18 17:21:56 +0800

    Update 3_init_conjur_cert_authority.sh

--- 3_init_conjur_cert_authority.sh
+++ 3_init_conjur_cert_authority.sh
@@ -3,16 +3,20 @@ set -euo pipefail
 
 . utils.sh
 
-announce "Initializing Conjur certificate authority."
+announce "Skipping Initialize Conjur certificate authority."
 
-set_namespace $CONJUR_NAMESPACE_NAME
+#
+#  no need to use Conjur CA for OSS
+#
 
-conjur_master=$(get_master_pod_name)
+#set_namespace $CONJUR_NAMESPACE_NAME
 
-if [ $CONJUR_VERSION = '4' ]; then
-  $cli exec $conjur_master -- conjur-plugin-service authn-k8s rake ca:initialize["conjur/authn-k8s/$AUTHENTICATOR_ID"] > /dev/null
-elif [ $CONJUR_VERSION = '5' ]; then
-  $cli exec $conjur_master -- chpst -u conjur conjur-plugin-service possum rake authn_k8s:ca_init["conjur/authn-k8s/$AUTHENTICATOR_ID"]
-fi
+#conjur_master=$(get_master_pod_name)
+
+#if [ $CONJUR_VERSION = '4' ]; then
+#  $cli exec $conjur_master -- conjur-plugin-service authn-k8s rake ca:initialize["conjur/authn-k8s/$AUTHENTICATOR_ID"] > /dev/null
+#elif [ $CONJUR_VERSION = '5' ]; then
+#  $cli exec $conjur_master -- chpst -u conjur conjur-plugin-service possum rake authn_k8s:ca_init["conjur/authn-k8s/$AUTHENTICATOR_ID"]
+#fi
 
 echo "Certificate authority initialized."
