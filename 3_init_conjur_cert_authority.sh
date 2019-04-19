e5375e7b5f42ee3c5192d6441c0fbbe8281263b6
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-04-20 00:48:43 +0800

    Update 3_init_conjur_cert_authority.sh

--- 3_init_conjur_cert_authority.sh
+++ 3_init_conjur_cert_authority.sh
@@ -9,8 +9,8 @@ set_namespace $CONJUR_NAMESPACE_NAME
 
 chmod +x ./initCA.sh
 
-$cli cp ./initCA.sh $$cli_pod_name:/root
-
-$cli exec $$cli_pod_name -- /root/initCA.sh $AUTHENTICATOR_ID $CONJUR_ACCOUNT
+$cli cp ./initCA.sh $cli_pod_name:/root
+$cli exec -it $cli_pod_name -- conjur authn login -u admin -p $CONJUR_ADMIN_PASSWORD
+$cli exec $cli_pod_name -- /root/initCA.sh $AUTHENTICATOR_ID $CONJUR_ACCOUNT
 
 echo "Certificate authority initialized."
