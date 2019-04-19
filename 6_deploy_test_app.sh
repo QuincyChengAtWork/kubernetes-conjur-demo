6ad478a80be207ab50fd6aef349cd7a6db7085a2
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-04-19 16:42:52 +0800

    Update 6_deploy_test_app.sh

--- 6_deploy_test_app.sh
+++ 6_deploy_test_app.sh
@@ -62,8 +62,12 @@ init_connection_specs() {
     secretless_image="cyberark/secretless-broker"
   fi
 
-  conjur_appliance_url=https://conjur-follower.$CONJUR_NAMESPACE_NAME.svc.cluster.local/api
-  conjur_authenticator_url=https://conjur-follower.$CONJUR_NAMESPACE_NAME.svc.cluster.local/api/authn-k8s/$AUTHENTICATOR_ID
+
+conjur_appliance_url=https://$conjur_service/api
+conjur_appliance_url=https://$conjur_service/api/authn-k8s/$AUTHENTICATOR_ID
+
+#  conjur_appliance_url=https://conjur-follower.$CONJUR_NAMESPACE_NAME.svc.cluster.local/api
+#  conjur_authenticator_url=https://conjur-follower.$CONJUR_NAMESPACE_NAME.svc.cluster.local/api/authn-k8s/$AUTHENTICATOR_ID
 
   conjur_authn_login_prefix=""
   if [[ "$CONJUR_VERSION" == "4" ]]; then
