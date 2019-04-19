c96378430d6c60afa879abf2173c9bd6c2b05363
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-04-19 21:11:03 +0800

    Update 3_init_conjur_cert_authority.sh

--- 3_init_conjur_cert_authority.sh
+++ 3_init_conjur_cert_authority.sh
@@ -5,18 +5,43 @@ set -euo pipefail
 
 announce "Initializing Conjur certificate authority."
 
-
 set_namespace $CONJUR_NAMESPACE_NAME 
 
+$cli exec $conjur_pod_name -- export AUTHENTICATOR_ID=$AUTHENTICATOR_ID
+$cli exec $conjur_pod_name -- export CONJUR_ACCOUNT=$CONJUR_ACCOUNT
 
-$cli exec $conjur_pod_name bundle exec rake authn_k8s:ca_init["conjur/authn-k8s/$AUTHENTICATOR_ID"]
+$cli exec $conjur_pod_name -- cat >>/root/initCA.sh<<EOF
+#!/bin/bash
+set -e
+#AUTHENTICATOR_ID='<AUTHENTICATOR_ID>'
+#CONJUR_ACCOUNT='<CONJUR_ACCOUNT>'
 
-#conjur_master=$(get_master_pod_name)
+openssl genrsa -out ca.key 2048
 
-#if [ $CONJUR_VERSION = '4' ]; then
-#  $cli exec $conjur_master -- conjur-plugin-service authn-k8s rake ca:initialize["conjur/authn-k8s/$AUTHENTICATOR_ID"] > /dev/null
-#elif [ $CONJUR_VERSION = '5' ]; then
-#  $cli exec $conjur_master -- chpst -u conjur conjur-plugin-service possum rake authn_k8s:ca_init["conjur/authn-k8s/$AUTHENTICATOR_ID"]
-#fi
+CONFIG="
+[ req ]
+distinguished_name = dn
+x509_extensions = v3_ca
+[ dn ]
+[ v3_ca ]
+basicConstraints = critical,CA:TRUE
+subjectKeyIdentifier   = hash
+authorityKeyIdentifier = keyid:always,issuer:always
+"
+
+# Generate root CA certificate
+openssl req -x509 -new -nodes -key ca.key -sha1 -days 3650 -set_serial 0x0 -out ca.cert \
+  -subj "/CN=conjur.authn-k8s.$AUTHENTICATOR_ID/OU=Conjur Kubernetes CA/O=$CONJUR_ACCOUNT" \
+  -config <(echo "$CONFIG")
+
+# Verify cert
+openssl x509 -in ca.cert -text -noout
+
+# Load variable values
+conjur variable values add conjur/authn-k8s/$AUTHENTICATOR_ID/ca/key "$(cat ca.key)"
+conjur variable values add conjur/authn-k8s/$AUTHENTICATOR_ID/ca/cert "$(cat ca.cert)"
+EOF
+
+$cli exec $conjur_pod_name -- /root/initCA.sh
 
 echo "Certificate authority initialized."
